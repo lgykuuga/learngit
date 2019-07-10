a07a99e6df1df69de3a68a77253e8c72c20d6972
lgykuuga <645241426@qq.com>
2019-07-10 20:50:41 +0800

    其它

--- src/main/java/com/lgy/learngit/LearngitApplication.java
+++ src/main/java/com/lgy/learngit/LearngitApplication.java
@@ -2,11 +2,13 @@ package com.lgy.learngit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class LearngitApplication {
 
     public static void main(String[] args) {
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(LearngitApplication.class);
         SpringApplication.run(LearngitApplication.class, args);
     }
 
