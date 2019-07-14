345e0d96841008c1095dc3b4369310506d8b18a3
lgykuuga <645241426@qq.com>
2019-07-14 20:23:38 +0800

    leecode

---
+++ src/main/java/com/lgy/leetcode/ToLowerCase.java
@@ -0,0 +1,11 @@
+package com.lgy.leetcode;
+
+public class ToLowerCase {
+
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("HHHHHH"));
+    }
+    public static String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+}
