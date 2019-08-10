f3cfb44ada909c4afd705793e0cbfb7b769e011c
lgykuuga <645241426@qq.com>
2019-08-10 18:23:50 +0800

    反转list

--- src/main/java/com/lgy/leetcode/Generate.java
+++ src/main/java/com/lgy/leetcode/Generate.java
@@ -14,6 +14,13 @@ import java.util.List;
  *  [1,4,6,4,1]
  * ]
  *
+ *
+ * 1
+ * 1  1
+ * 1  2  1
+ * 1  3  3  1
+ * 1  4  6  4  1
+ *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/pascals-triangle
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -26,32 +33,37 @@ public class Generate {
 
     public List<List<Integer>> generate(int numRows) {
 
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> triangle = new ArrayList<List<Integer>>();
 
+        // First base case; if user requests zero rows, they get zero rows.
         if (numRows == 0) {
-            return result;
+            return triangle;
         }
 
-        if (numRows == 1) {
-            result.get(0).add(1);
-        }
+        // Second base case; first row is always [1].
+        triangle.add(new ArrayList<>());
+        triangle.get(0).add(1);
 
-        for (int i = 1; i < numRows; i++) {
-            List<Integer> row = new ArrayList<>();//行
+        for (int rowNum = 1; rowNum < numRows; rowNum++) {
+            List<Integer> row = new ArrayList<>();
+            List<Integer> prevRow = triangle.get(rowNum-1);
 
+            // The first row element is always 1.
             row.add(1);
 
-            for (int j = 0; j < numRows - 2; j++) {
-
-
-
+            // Each triangle element (other than the first and last of each row)
+            // is equal to the sum of the elements above-and-to-the-left and
+            // above-and-to-the-right.
+            for (int j = 1; j < rowNum; j++) {
+                row.add(prevRow.get(j-1) + prevRow.get(j));
             }
 
+            // The last row element is always 1.
             row.add(1);
 
+            triangle.add(row);
         }
 
-
-        return result;
+        return triangle;
     }
 }
