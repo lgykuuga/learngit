9138240af3c2132711dd9bd7a82795c9d8d6e7c3
lgykuuga <645241426@qq.com>
2019-08-31 21:07:49 +0800

    leetcode

---
+++ src/main/java/com/lgy/leetcode/easy/MiddleNode.java
@@ -0,0 +1,86 @@
+package com.lgy.leetcode.easy;
+
+/**
+ * 给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+ *
+ * 如果有两个中间结点，则返回第二个中间结点。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：[1,2,3,4,5]
+ * 输出：此列表中的结点 3 (序列化形式：[3,4,5])
+ * 返回的结点值为 3 。 (测评系统对该结点序列化表述是 [3,4,5])。
+ * 注意，我们返回了一个 ListNode 类型的对象 ans，这样：
+ * ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next = NULL.
+ *
+ * 示例 2：
+ *
+ * 输入：[1,2,3,4,5,6]
+ * 输出：此列表中的结点 4 (序列化形式：[4,5,6])
+ * 由于该列表有两个中间结点，值分别为 3 和 4，我们返回第二个结点。
+ *
+ *
+ *
+ * 提示：
+ *
+ *     给定链表的结点数介于 1 和 100 之间。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/middle-of-the-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MiddleNode {
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        ListNode listNode = middleNode1(listNode1);
+        while (listNode.next != null) {
+            System.out.println(listNode.val);
+            listNode = listNode.next;
+        }
+        System.out.println(listNode.val);
+    }
+
+    /**
+     * TODO 未通过
+     * @param head
+     * @return
+     */
+    public static ListNode middleNode1(ListNode head) {
+        ListNode[] listNodes = new ListNode[100];
+        int index = 0;
+        while (head.next != null) {
+            listNodes[index++] = head;
+            head = head.next;
+        }
+        if (index%2 == 0) {
+            return listNodes[index/2 + 1];
+        }
+        return listNodes[index/2];
+    }
+
+    /**
+     * 快慢指针
+     * @param head
+     * @return
+     */
+    public static ListNode middleNode2(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
